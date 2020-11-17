d0cffb20b2e06882471adae0078947afb471fb25
JosMuse <no-reply>
2020-11-17 14:14:25 -0700

    added server file

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+//routes
+
+require("./routes/api-routes.js")(app)
+require("./routes/html-routes.js")(app)
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
